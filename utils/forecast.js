5e8b6a306041a7b160b94886f254ef6c8e7008da
Divya <dsrao0712@gmail.com>
2021-09-01 20:57:13 +0530

    Object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,17 +4,19 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=242f91bf2389cb2098212e5f800f7c2e&query=' + latitude + ',' + longitude
 
-    request({ url: url, json: true }, (error, response) => {
+
+    // Using object destructuring 
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather servies ', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location. Please try again', undefined)
         } else {
             callback(undefined, {
-                temperature: response.body.current.temperature,
-                description: response.body.current.weather_descriptions[0],
-                feels_like: response.body.current.feelslike,
-                humidity: response.body.current.humidity
+                temperature: body.current.temperature,
+                description: body.current.weather_descriptions[0],
+                feels_like: body.current.feelslike,
+                humidity: body.current.humidity
             })
         }
     })
