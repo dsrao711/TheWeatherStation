14dad912dfd9ddc67b078a240353d5cdf6bd8120
Divya <dsrao0712@gmail.com>
2021-09-10 20:55:48 +0530

    default parameterrs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express()
 const hbs = require('hbs')
 
 
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+
 publicDirPath = path.join(__dirname, '../public')
 viewsDirPath = path.join(__dirname, '../templates/views')
 partialsDirPath = path.join(__dirname, '../templates/partials')
@@ -23,7 +28,33 @@ app.get('', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
-    res.send('Weather data')
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide a location for weather updates"
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error: error
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+
 })
 
 
